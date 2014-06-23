a99866e7dda3437f67c1819b1ac5b5347c27326d
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-06-23 20:43:12 +0200

    Force reflow in Chrome until flexbox render issue is fixed. Fixes #12

--- app/js/app.js
+++ app/js/app.js
@@ -122,6 +122,11 @@ app.controller('PlacesController', ['$scope', '$http', function($scope, $http) {
 
             storeData();
 
+            // Force reflow in Chrome until flexbox render issue is fixed
+            var elmList = document.querySelector('.places');
+            elmList.style.display='none';
+            elmList.style.display='block';
+
         });
     }
 
