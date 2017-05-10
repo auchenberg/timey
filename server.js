18449850ebeebb809134d21632992d82fd366e2a
Kenneth Auchenberg <kenneth@auchenberg.dk>
2017-05-10 09:45:43 -0700

    Remove comments

--- server.js
+++ server.js
@@ -16,15 +16,9 @@ app.use(lessMiddleware(__dirname + '/app', {
   force: true
 }))
 
-// Step 2: Attach the dev middleware to the compiler & the server
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true, publicPath: webpackConfig.output.publicPath
 }))
-
-// // Step 3: Attach the hot middleware to the compiler & the server
-// app.use(require("webpack-hot-middleware")(compiler, {
-//   log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-// }))
 
 app.use(express.static(__dirname + '/app'))
 
