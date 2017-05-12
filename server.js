41cbe2e4fc8758072dbcd152d45f37363a36fe0f
Kenneth Auchenberg <kenneth@auchenberg.dk>
2017-05-12 10:50:00 -0700

    Serve static files in PROD from Node server

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.set('port', (process.env.PORT|| 3001));
 
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.get('/api/timezone', (req, res) => {
 
   request(`https://timezoneapi.io/api/address/?${req.query.place}`, (err, param, response) => {
